64fe2d9ae04a1eb7d942685952b001437193e965
Hard Bosamiya <haardbosamiya4@gmail.com>
2025-04-22 21:45:50 +0530

    Fix package.json and confirm index.js location

--- src/index.js
+++ src/index.js
@@ -421,7 +421,7 @@ app.post("/delete", async (req, res) => {
 
 
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running on Port: ${port}`);
 });
